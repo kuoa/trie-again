0294b203ffc6c8e233b5ec808a62dd4663b4af02
Aymeric ROBINI <aymericrobini@laposte.net>
2016-11-20 23:23:39 +0100

    Add some stuff on HybridTrie

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,12 +1,44 @@
+import datastructures.HybridTrie;
 import datastructures.PatriciaTrie;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.List;
 
 
 public class TestMain {
-
 	public static void main(String[] args) {
+		final String[] words2 = new String[]{
+                "A", "quel", "genial", "professeur", "de", "dactylographie", "sommes", "nous", "redevables", "de",
+                "la", "superbe", "phrase", "ci", "dessus", ",", "un", "modele", "du", "genre", ",", "que", "toute",
+                "dactylo", "connait", "par", "coeur", "puisque", "elle", "fait", "appel", "a", "chaqune", "des",
+                "touches", "du", "clavier", "de", "la", "machine", "a", "ecrire", "?" };
+		final String[] words = new String[]{
+				"bonjour", "banane", "atome", "at"
+		};
+		
+		HybridTrie ht = new HybridTrie();
+		for(String w : words) {
+			ht.insert(w);
+		}
+		
+//		System.out.println(ht.lookup("banen"));
+//		System.out.println(ht.lookup("banane"));
+//		System.out.println(ht.lookup("at"));
+//		System.out.println(ht.lookup("ati"));
+//		System.out.println(ht.lookup("atome"));
+//		for(; ht != null; ht = ht.middle) {
+//			System.out.print(ht.letter);
+//		}
+//		System.out.println(ht.middle.middle.right.middle.middle.letter);
+		
+		System.out.println(ht.draw());
+		
+	}
+}
+
+
+class TestMainPT {
+
+	public static void main2(String[] args) {
 
         final String[] words = new String[]{
                 "A", "quel", "genial", "professeur", "de", "dactylographie", "sommes", "nous", "redevables", "de",
@@ -16,9 +48,6 @@ public class TestMain {
 
 
         PatriciaTrie trie = new PatriciaTrie();
-
-
-
 
         int count = 0, oldCount = count;
 
@@ -47,10 +76,6 @@ public class TestMain {
         System.out.println("prefixed by");
         System.out.println(trie.nbPrefixed("de"));
         System.out.println(trie.draw());
-        System.out.println("height " + trie.height());
-        System.out.println("avg depth " + trie.avgDepth());
-
-
 
 
 
@@ -69,6 +94,7 @@ public class TestMain {
         trie.insert("ho");
 		System.out.println(trie);
         System.out.println(trie.count());
+        System.out.println(trie.height());
 
         List<String> listWords = trie.listWords();
         for(String w : listWords){
@@ -77,8 +103,9 @@ public class TestMain {
 
         System.out.println(trie.nbPrefixed("ho"));
         System.out.println(trie.draw());
-        System.out.println("height " + trie.height());
-        System.out.println("avg depth " + trie.avgDepth());
+
+
+
 */
         //remove
         /*
--- src/datastructures/HybridTrie.java
+++ src/datastructures/HybridTrie.java
@@ -3,16 +3,55 @@ package datastructures;
 import interfaces.ITrie;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HybridTrie implements ITrie {
 	
 	public HybridTrie left, right, middle;
 	public char letter;
+	
+	private boolean isNull;
+		
+	public HybridTrie() {
+		isNull = true;
+	}
+	
+	public static HybridTrie nullHT() {
+		return new HybridTrie();
+	}
+	
+	public void set(char l) {
+		isNull = false;
+		letter = l;
+		left = nullHT();
+		middle = nullHT();
+		right = nullHT();
+	}
 
-	@Override
 	public void insert(String word) {
-		// TODO Auto-generated method stub
-
+		_insert(word+"#");
+	}
+	
+	private void _insert(String word) {
+		if(word.isEmpty()) return;
+		
+		char l = word.charAt(0);
+		if(isNull) {
+			set(l);
+			middle._insert(word.substring(1));
+			return;
+		}
+		
+		if(l < letter) {
+			left._insert(word);
+		}
+		else if(l > letter) {
+			right._insert(word);
+		} else {
+			middle._insert(word.substring(1));
+		}
+				
+		
 	}
 
 	@Override
@@ -23,8 +62,24 @@ public class HybridTrie implements ITrie {
 
 	@Override
 	public boolean lookup(String word) {
-		// TODO Auto-generated method stub
-		return false;
+		return _lookup(word+"#");
+	}
+	
+	public boolean _lookup(String word) {
+		if(isNull) return false;
+		
+		if(word.isEmpty())
+			return true;
+		
+		char l = word.charAt(0);
+		
+		if(letter == l) {
+			return middle._lookup(word.substring(1));
+		} else if(l < letter) {
+			return left._lookup(word);
+		} else {
+			return right._lookup(word);
+		}
 	}
 
 	@Override
@@ -77,7 +132,45 @@ public class HybridTrie implements ITrie {
 
 	@Override
 	public String draw() {
-		return null;
+		return "digraph T {\n" + _draw(new AtomicInteger(0), new StringBuilder(""), 0).toString() + "}";
+	}
+	
+	private StringBuilder _draw(AtomicInteger nc, StringBuilder sb, int father) {
+		String me = Integer.toString(father);
+		
+		if(isNull) {
+			int id = nc.incrementAndGet();
+			sb.append("null"+ id + "[shape=point]\n");
+			sb.append(me +" -> null"+ id +"\n");
+			return sb;
+		}
+		
+		sb.append(me + "[label = \""+letter+"\"]\n");
+		
+		int ncl, ncm, ncr;
+		
+		ncl = nc.incrementAndGet();
+		sb.append(me + " -> " + ncl + "\n");//[label=\""+String.valueOf(left.letter)+"\"]\n");
+		
+		ncm = nc.incrementAndGet();
+		sb.append(me + " -> " + ncm + "\n");//[label=\""+String.valueOf(left.letter)+"\"]\n");
+		
+		ncr = nc.incrementAndGet();
+		sb.append(me + " -> " + ncr + "\n");
+		
+		left._draw(nc, sb, ncl);
+		middle._draw(nc, sb, ncm);
+		right._draw(nc, sb, ncr);
+		
+		return sb;
+	}
+	
+	public String toString() {
+		if(isNull) {
+			return "<>";
+		} else {
+			return "["+letter+"]("+left.toString()+", "+middle.toString()+", "+right.toString()+")";
+		}
 	}
 
 }
