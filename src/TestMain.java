27bcf63bd2a377c193c0a3aa7205cc8c9f5b650e
Aymeric ROBINI <aymericrobini@laposte.net>
2016-11-24 19:21:23 +0100

    Add some benchmarks

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,24 +1,53 @@
 import datastructures.HybridTrie;
 import datastructures.PatriciaTrie;
+import interfaces.ITrie;
 
 import java.util.List;
 
 
 public class TestMain {
 	public static void main(String[] args) {
-		final String[] words2 = new String[]{
-                "A", "quel", "genial", "professeur", "de", "dactylographie", "sommes", "nous", "redevables", "de",
-                "la", "superbe", "phrase", "ci", "dessus", ",", "un", "modele", "du", "genre", ",", "que", "toute",
-                "dactylo", "connait", "par", "coeur", "puisque", "elle", "fait", "appel", "a", "chaqune", "des",
-                "touches", "du", "clavier", "de", "la", "machine", "a", "ecrire", "?" };
-		final String[] words = new String[]{
-				"bonjour", "banane", "baleine", "atome", "at", "c", "bateau"
-		};
+		/**** c'est un peu brouillon, faudra surement modifier un peu des trucs ****/
+		Benchmark b = new Benchmark();
+		ITrie pt = null, ht = null;
 		
-		HybridTrie ht = new HybridTrie();
-		for(String w : words) {
-			ht.insert(w);
+		for(int i=1; i <= 5; i++) {
+			b.initBench(new HybridTrie());
+			b.insertBench("/users/nfs/Etu3/3300543/workspace/trie-again/data/test"+i+".in");
+			System.out.println("Reading a total of "+b.getnChars()+" chars in Hybrid, time : "+b.getElapsed()+" µs");
+			ht = b.getTree();
 		}
+		
+		for(int i=1; i <= 5; i++) {
+			b.initBench(new PatriciaTrie());
+			b.insertBench("/users/nfs/Etu3/3300543/workspace/trie-again/data/test"+i+".in");
+			System.out.println("Reading a total of "+b.getnChars()+" chars in PATRICIA, time : "+b.getElapsed()+" µs");
+			pt = b.getTree();
+		}
+		
+		b.initBench(null);
+		b.lookupBench("/users/nfs/Etu3/3300543/workspace/trie-again/data/test5.in", pt);
+		System.out.println("Looking for "+b.getnChars()+" chars in PATRICIA, time : "+b.getElapsed()+" µs");
+		
+		b.initBench(null);
+		b.lookupBench("/users/nfs/Etu3/3300543/workspace/trie-again/data/test5.in", ht);
+		System.out.println("Looking for "+b.getnChars()+" chars in Hybrid, time : "+b.getElapsed()+" µs");		
+	
+		
+		
+//		final String[] words2 = new String[]{
+//                "A", "quel", "genial", "professeur", "de", "dactylographie", "sommes", "nous", "redevables", "de",
+//                "la", "superbe", "phrase", "ci", "dessus", ",", "un", "modele", "du", "genre", ",", "que", "toute",
+//                "dactylo", "connait", "par", "coeur", "puisque", "elle", "fait", "appel", "a", "chaqune", "des",
+//                "touches", "du", "clavier", "de", "la", "machine", "a", "ecrire", "?" };
+//		final String[] words = new String[]{
+//				"bonjour", "banane", "baleine", "atome", "at", "c", "bateau"
+//		};
+//		
+//		HybridTrie ht = new HybridTrie();
+//		for(String w : words) {
+//			ht.insert(w);
+//		}
 		
 //		System.out.println(ht);
 		
@@ -45,12 +74,12 @@ public class TestMain {
 //		System.out.println("#words starting by 'a' = "+ht.nbPrefixed("a"));
 //		System.out.println("#words starting by 'ki' = "+ht.nbPrefixed("ki"));
 		
-		ht.remove("banane");
-		ht.remove("atome");
-		ht.remove("hello");
-		ht.remove("c");
-		
-		System.out.println(ht.listWords());
+//		ht.remove("banane");
+//		ht.remove("atome");
+//		ht.remove("hello");
+//		ht.remove("c");
+//		
+//		System.out.println(ht.listWords());
 	}
 }
 
